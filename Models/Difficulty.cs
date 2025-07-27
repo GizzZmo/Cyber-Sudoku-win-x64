8300f5ecc5c3b493f60e9f1120ed0c196382b970
Jon Arve Ovesen <jonovesen@gmail.com>
2025-07-27 06:23:26 +0200

    Create Difficulty.cs

---
+++ Models/Difficulty.cs
@@ -0,0 +1,11 @@
+// --- File: Models/Difficulty.cs ---
+// Defines the difficulty levels of the puzzles.
+namespace CyberSudoku.MAUI.Models;
+
+public enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard,
+    Expert
+}
